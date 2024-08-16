1921b962058ddd2306323090cac889af1d27854e
Tam309 <t3doch00@students.oamk.fi>
2024-08-16 12:55:01 +0300

    add index.js

---
+++ back_end/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const { userRouter } = require('./routes/user.js');
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+app.use('/',userRouter)
+
+const port = process.env.PORT;
+
+app.listen(port)
