9fff65ef82ac8c6fe1f96089500568951f674b66
Tam309 <t3doch00@students.oamk.fi>
2024-08-16 12:46:19 +0300

    update user.js

--- back_end/routes/user.js
+++ back_end/routes/user.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 const userRouter = express.Router();
 
-//Register
+// Register
 userRouter.post("/register", async (req, res) => {
     bcrypt.hash(req.body.password, 10, async (err, hash) => {
         if(!err) {
@@ -12,7 +12,7 @@ userRouter.post("/register", async (req, res) => {
             const email = req.body.email;
             const password = hash;
             try {
-                const sql = 'INSERT INTO users (user_name, password, email, avatar) VALUES ($1, $2, $3, (SELECT id FROM avatars ORDER BY random() LIMIT 1)) RETURNING *'
+                const sql = 'INSERT INTO users (user_name, password, email) VALUES ($1, $2, $3) RETURNING *'
                 const result = await query(sql, [user_name, password, email]);
                 const rows = result.rows ? result.rows : [];
                 res.status(200).json({ user_id: rows[0].user_id, user_name: user_name, email: email, avatar: rows[0].avatar })
@@ -27,43 +27,36 @@ userRouter.post("/register", async (req, res) => {
         }
     });
 });
-
-//Login to an account:
-userRouter.post("/users/login", async(req,res) => {
-    console.log(req.body);
-    try {
-        const sql = "SELECT users.*, avatars.link FROM users INNER JOIN avatars ON users.avatar=avatars.id WHERE email=$1";
-        const result = await query(sql,[req.body.email]);
-        console.log(result.rowCount)
-        console.log(req.body.password);
-        console.log("This " + result.rows[0].password);
-        if (result.rowCount === 1) {
-            bcrypt.compare(req.body.password,result.rows[0].password, (err, bcrypt_res) => {
-                if (!err) {
-                    console.log('ok')
+// Login
+userRouter.post("/login", async (req, res) => {
+    try{
+        const sql = "select * from users where email = $1";
+        const result = await query(sql, [req.body.email]);
+        if (result.rowCount === 1){
+            bcrypt.compare(req.body.password, result.rows[0].password, (err, bcrypt_res) => {
+                if(!err){
                     console.log(bcrypt_res)
-                    if (bcrypt_res === true) {
+                    if(bcrypt_res === true){
                         const user = result.rows[0];
-                        res.status(200).json({ "user_id": user.user_id,"email": user.email, "user_name": user.user_name, "avatar": user.avatar, "link": user.link })
-                    } else {
-                        res.statusMessage = 'Invalid login';
-                        res.status(401).json({ error: 'Invalid login '})
+                        res.status(200).json({"user_id": user.user_id, "email": user.email, "user_name": user.user_name})
+                    }else{
+                        res.statusMessage = "Invalid login";
+                        res.status(401).json({error: 'Invalid login'})
                     }
-                } else {
+                }else{
                     res.statusMessage = err;
                     res.status(500).json({error: err})
                 }
             })
-        } else {
-            res.statusMessage = 'Invalid login';
+        } else{
+            res.statusMessage = "User not found";
             res.status(401).json({error: 'Invalid login'})
-        }
-    } catch (error) {
-        res.statusMessage = error;
-        res.status(500).json({error: error})
+        } 
+    }catch(err){
+        res.statusMessage = err;
+        res.status(500).json({error: err})
     }
 })
-
 // Change password
 userRouter.put("/users/password", async(req, res) => {
     console.log(req.body);
@@ -94,21 +87,4 @@ userRouter.put("/users/password", async(req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-
-// User page 
-userRouter.get("/user/:user_id", async (req, res) => {
-    const user_id = Number(req.params.user_id);
-    try {
-        const userResult = await query('SELECT users.*, avatars.link FROM users INNER JOIN avatars ON users.avatar=avatars.id WHERE user_id = $1', [user_id]);
-        const postResult = await query('SELECT post_id FROM posts WHERE user_id = $1', [user_id]);
-        
-        const userData = userResult.rows ? userResult.rows[0] : {};
-        const postIds = postResult.rows ? postResult.rows.map(post => post.post_id) : [];
-
-        res.status(200).json({ user: userData, posts: postIds });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
 module.exports = { userRouter };
