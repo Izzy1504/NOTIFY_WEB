bae61fd2012638d569944d5fd0decab7d8fcb2ce
Tam309 <t3doch00@students.oamk.fi>
2024-08-19 15:29:20 +0300

    add signup page

---
+++ src/Component/Signup/Signup.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Signup.css'
+
+const signupForm = () => {
+    return (
+        <div className="signup-page">
+            <div className="signup-container">
+                <form className="signup-form">
+                    <h2>Signup</h2>
+                    <div className="input-group">
+                        <input type="text" placeholder="Email" />
+                    </div>
+                    <div className="input-group">
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <div className="input-group">
+                        <input type="password" placeholder="Confirm Password" />
+                    </div>
+                    <button className="signup-btn">Signup</button>
+                    <div className="social-login">
+                        <p>Or signup with</p>
+                        <div className="social-buttons">
+                            <div className="social-btn facebook">Facebook</div>
+                            <div className="social-btn google">Google</div>
+                        </div>
+                    </div>
+                    <div className="login-text">
+                        <p>Already a member? <a href="#">Login now</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
